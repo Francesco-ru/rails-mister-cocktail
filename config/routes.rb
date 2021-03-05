14310e2037336f1882ff9bf9ecfcb9cdcbc22ea2
Francesco Rulli <rulli.francesco1997@gmail.com>
2021-03-05 11:03:42 +0100

    francescoru-mrcocktails

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
+  get '/search', to: 'searches#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
